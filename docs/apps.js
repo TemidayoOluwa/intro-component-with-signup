7190a335c635f6c2f3e13f864bf1d76d75b7f37b
Temidayo Oluwagbemiga <esthertemidayo82@gmail.com>
2024-05-29 06:39:17 +0100

    js file updated

--- docs/apps.js
+++ docs/apps.js
@@ -1,3 +1,39 @@
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form from submitting normally
+
+  validateInputs(); // Call the function to validate inputs
+});
+
+function validateInputs() {
+  const inputs = document.querySelectorAll('.general');
+  inputs.forEach(input => {
+      const value = input.value.trim();
+      const parent = input.closest('.cover');
+      const errorMessage = parent.querySelector('.error-message');
+      const errorIcon = parent.querySelector('.error-icon');
+      console.log(errorIcon)
+      if (value === '') {
+          setError(parent, errorMessage, errorIcon, `${input.placeholder} cannot be empty`);
+      } else {
+          setSuccess(parent, errorMessage, errorIcon);
+      }
+  });
+}
+const parent = input.closest('.cover');
+// const inputs= document.querySelector('.blocks');
+function setError(parent, errorMessage, errorIcon, message) {
+  parent.classList.add('error');
+  errorMessage.textContent = message;
+  errorMessage.style.display = 'block';
+  errorIcon.style.display = 'flex';
+}
+
+function setSuccess(parent, errorMessage, errorIcon) {
+  parent.classList.remove('error');
+  errorMessage.textContent = '';
+  errorMessage.style.display = 'none';
+  errorIcon.style.display = 'none';
+}
 
 
 // the toggle eye button
